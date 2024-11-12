da17c41339138fec99e3ef5eabc25fbcd618e51d
wawa87 <guthrie.anthony@gmail.com>
2024-11-12 08:07:17 -0800

    Add CredentialHandler for digesting passwords.

--- src/main/resources/dbSchema/schema.sql
+++ src/main/resources/dbSchema/schema.sql
@@ -13,7 +13,7 @@ DELETE FROM user_roles WHERE 1;
 DELETE FROM users WHERE 1;
 
 INSERT INTO users (username, password)
-VALUES ('devuser', 'devpass');
+VALUES ('devuser', 'dc067f8a150df19383bc33d7ac9032f7');
 
 INSERT INTO user_roles (username, role_name)
 VALUES ('devuser', 'SIMPLEUSER');
